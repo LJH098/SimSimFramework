43e3b0dfe68d897b27315a065de3ea71e0f61bcb
Jin Hyuk <ekrrdj07@gmail.com>
2025-05-21 15:10:21 +0900

    test: 테스트 샘플 수정

--- src/main/java/org/example/SimSimFramework.java
+++ src/main/java/org/example/SimSimFramework.java
@@ -1,19 +1,17 @@
 package org.example;
 
-import org.example.bean.BeanContainer;
-import org.example.component.ComponentScanner;
+import org.example.applicationcontext.ApplicationContext;
+import org.example.config.AppConfig;
 import org.example.test.TestService;
 
 public class SimSimFramework {
     public static void main(String[] args) throws Exception{
         try{
-            BeanContainer container = new BeanContainer();
-            ComponentScanner scanner = new ComponentScanner(container);
-            scanner.scan("org.example"); // 패키지 전체 스캔
+            ApplicationContext context = new ApplicationContext(AppConfig.class);
 
-            TestService testService = container.getBean(TestService.class);
+            TestService testService = context.getBean(TestService.class);
             testService.doSomething();
-            container.close();
+            context.close();
 
         }catch(Exception e){
             e.printStackTrace();
